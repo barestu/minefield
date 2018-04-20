760392bf1919ee0ec478adc3060f853b20b6ae27
Fitrul Islam <fitrul.islam@gmail.com>
2018-04-20 07:30:51 +0700

    add logic

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import '@/assets/js/firebase.js'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueFire from 'vuefire'
 import '@/assets/css/bootstrap.min.css'
 
+Vue.use(VueFire)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,83 @@
 <template>
   <div class="home">
     <div class="container">
-      <div class="row ml-auto">
-        <div :key="index" v-for="index in 16" class="col-3" id="fieldCard">
-          <Card></Card>
+      <div v-if="show" class="row ml-auto">
+        <div v-bind:key="index" v-for="(board, index) in boards" class="col-3" id="fieldCard">
+          <Card v-bind:board="board"></Card>
         </div>
       </div>
     </div>
+    <!-- <button type="button" @click="play(boards)">Play</button> -->
+    <button type="button" @click="reset()">Reset</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Card from '@/components/Card.vue'
+import { boardRef } from '@/assets/js/firebase.js'
 
 export default {
   name: 'home',
   components: {
     Card
+  },
+  data () {
+    return {
+      name: '',
+      show: false
+    }
+  },
+  firebase: {
+    boards: boardRef
+  },
+  created: function () {
+  //   this.$store.commit('showcard',this.boards)
+    this.play(this.boards)
+  },
+  updated: {
+    // this.boards
+  },
+  methods: {
+    addData () {
+      boardRef.push({
+        status: 'safeToHit',
+        isAlive: false
+      })
+    },
+    play (array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
+        let temp = array[i]
+        let key1 = array[i]['.key']
+        let key2 = array[j]['.key']
+        boardRef.child(key1).update({status: array[j].status, isAlive: array[j].isAlive})
+        boardRef.child(key2).update({status: temp.status, isAlive: temp.isAlive})
+      }
+      this.show = true
+    },
+    reset () {
+      this.boards.forEach(board => {
+        let key = board['.key']
+        boardRef.child(key).update({isAlive: false})
+      })
+      this.show = false
+    },
+    changeStatus (data) {
+      let key = data['.key']
+      boardRef.child(key).update({isAlive: true})
+      if (data.status === 'bom') {
+        alert('woi ini bom')
+      }
+    }
   }
+
 }
 </script>
 
 <style scoped>
 .home {
-  margin: 0 210px;
+  /* margin: 0 210px; */
 }
 
 #fieldCard {
